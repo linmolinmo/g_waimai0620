8048c9c9c53efb54f0e7076435e8c9eb4eef89c7
wanglin <2842979502@qq.com>
2019-11-08 21:00:39 +0800

    oneday

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <router-view></router-view>
+     <FooterGuide/> 
+   
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
+
+export default {
+  name: 'App',
+  components: {FooterGuide}
+ 
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ 
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'lib-flexible/flexible'
+
+import router from './router'
+import GshopHeader from './components/GshopHeader/GshopHeader' 
+
+Vue.component('GshopHeader',GshopHeader)
+// Vue.components('GshopHeader',GshopHeader)//多写了一个components
+
+Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+
+new Vue({
+
+  components: {App},
+  template: '<App />',
+  router,
+
+ 
+}).$mount('#app')
+
+
